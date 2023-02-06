cc079c2f4b2f17575767da139901f629c78c33fa
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-06 09:33:12 +0200

    feat: hello world in yellow box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="justify-center items-center flex h-screen">
+      <div className="w-96 h-96 bg-yellow-500 flex items-center justify-center">
+        <h1>Hello world</h1>
+      </div>
     </div>
   );
 }
